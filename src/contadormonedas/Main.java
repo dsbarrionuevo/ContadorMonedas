ea44771a087708646c6cff1e57fdcee8ab0e5d54
Gochi <Gochi@Gochi-PC>
2014-10-27 16:01:08 -0300

    Elimino clase Imagen de Util y limpio Main.

--- src/contadormonedas/Main.java
+++ src/contadormonedas/Main.java
@@ -1,12 +1,5 @@
 package contadormonedas;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import util.Imagen;
 
 /**
  *
@@ -15,14 +8,6 @@ import util.Imagen;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            BufferedImage original = ImageIO.read(new File("./imagenes/img1.png"));
-            Imagen imagen = new Imagen(original);
-            imagen.escalaGrises(10);
-            imagen.guardar("./imagenes/_img1.png");
-        } catch (IOException ex) {
-            Logger.getLogger(Imagen.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 
 }
